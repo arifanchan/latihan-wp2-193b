1b46dc47f14b889e5267e5310c4686868638ccab
arifanchan <19215277@bsi.ac.id>
2023-06-17 21:18:46 +0700

    Menu Dashboard CI

--- application/libraries/template.php
+++ application/libraries/template.php
@@ -10,8 +10,8 @@ class Template
     function display($template, $data = null)
     {
         $data['_content'] = $this->_CI->load->view($template, $data, true);
-        //$data['_header']=$this->_CI->load->view('template/header',$data,true);
-        // $data['_sidebar']=$this->_CI->load->view('template/sidebar',$data,true);
+        $data['_header']=$this->_CI->load->view('template/header',$data,true);
+        $data['_sidebar']=$this->_CI->load->view('template/sidebar',$data,true);
         $this->_CI->load->view('/template.php', $data);
     }
 }
--- application/views/template2.php
+++ application/views/template2.php
@@ -5,9 +5,12 @@
     <title>Aplikasi Mahasiswa</title>
     <link href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/font-awesome/css/font-awesome.css'); ?>" rel="stylesheet">
+
     <link href="<?php echo base_url('assets/css/plugins/morris/morris-0.4.3.min.css'); ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/plugins/timeline/timeline.css'); ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/datepicker.css'); ?>" rel="stylesheet">
+
+
     <script src="<?php echo base_url('assets/js/jquery-1.8.3.min.js'); ?>"></script>
     <script src="<?php echo base_url('assets/js/bootstrap.js'); ?>"></script>
     <script src="<?php echo base_url('assets/js/tinymce/tinymce.min.js'); ?>"></script>
@@ -15,13 +18,16 @@
         tinymce.init({
             selector: 'textarea'
         });
+
         $(function() {
             $("#tanggal1").datepicker({
                 format: 'yyyy-mm-dd'
             });
+
             $("#tanggal2").datepicker({
                 format: 'yyyy-mm-dd'
             });
+
             $("#tanggal").datepicker({
                 format: 'yyyy-mm-dd'
             });
@@ -41,16 +47,21 @@
             </div>
         </div>
     </div>
+
     <div class="container">
         <div class="row">
-            <div class="col-md-3">
-                <?php echo $_sidebar; ?>
-            </div>
+            <!--
+                    <div class="col-md-3">
+                        <?php echo $_sidebar; ?>
+                    </div>
+					-->
+
             <div class="col-md-9">
                 <?php echo $_content; ?>
             </div>
         </div>
     </div>
+
     <!-- Modal -->
     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog">
@@ -70,6 +81,7 @@
             </div><!-- /.modal-content -->
         </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
+
     <!-- Core Scripts - Include with every page -->
     <script src="<?php echo base_url('assets/js/holder.js'); ?>"></script>
     <script src="<?php echo base_url('assets/js/bootstrap-datepicker.js'); ?>"></script>
